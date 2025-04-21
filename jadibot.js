1d6482be8479550e6f9ac1b25922c7c19edeebad
B.M.B <eniolaadebesin78@gmail.com>
2025-04-21 14:36:44 +0100

    Initial commit

---
+++ jadibot.js
@@ -0,0 +1,257 @@
+const { modul } = require('./module');
+const { baileys, boom, chalk, fs, figlet, FileType, process, PhoneNumber } = modul;
+const { Boom } = boom
+const path = require('path');
+const { default: makeWaSocket, useMultiFileAuthState, DisconnectReason, fetchLatestBaileysVersion, generateForwardMessageContent, generateWAMessage, prepareWAMessageMedia, generateWAMessageFromContent, generateMessageID, downloadContentFromMessage, makeInMemoryStore, jidDecode, proto, makeCacheableSignalKeyStore } = baileys
+const { color, bgcolor } = require('./lib/color')
+const log = (pino = require("pino"));
+const qrcode = require('qrcode');
+const rimraf = require("rimraf");
+let Pino = require("pino")
+
+const { imageToWebp, videoToWebp, writeExifImg, writeExifVid } = require('./lib/exif')
+const { smsg, isUrl, generateMessageTag, getBuffer, getSizeMedia, fetchJson, await, sleep, reSize } = require('./lib/myfunc')
+const owner = JSON.parse(fs.readFileSync('./database/owner.json').toString())
+const store = makeInMemoryStore({ logger: pino().child({ level: 'silent', stream: 'store' }) })
+   let NodeCache = require("node-cache")
+   let msgRetryCounterCache = new NodeCache() 
+if (global.conns instanceof Array) console.log()
+else global.conns = []
+
+const jadibot = async (Dare, text, m) => {
+const { sendImage, sendMessage } = Dare;
+//sendMessage(from, 'tesss');
+const { reply, sender } = m;
+const { state, saveCreds } = await useMultiFileAuthState(path.join(__dirname, `./database/rentbot/${sender}`), log({ level: "silent" }));
+try {
+async function start() {
+let { version, isLatest } = await fetchLatestBaileysVersion();
+ const config = {
+    logger: Pino({ level: "fatal" }).child({ level: "fatal" }),
+    printQRInTerminal: false,
+    mobile: false,
+    auth: {
+    creds: state.creds,
+    keys: makeCacheableSignalKeyStore(state.keys, Pino({ level: "fatal" }).child({ level: "fatal" })),
+    },
+    browser: [ "Ubuntu", "Chrome", "20.0.04" ],
+    markOnlineOnConnect: true,
+    generateHighQualityLinkPreview: true,
+    msgRetryCounterCache,
+    defaultQueryTimeoutMs: undefined
+ }
+ let simple = require('./index')
+if (!Dare.authState.creds.registered) {
+      setTimeout(async () => {
+         let phoneNumber = `${text}`  
+         console.log(chalk.red.bold(`[ Jadibot ] -> (+${phoneNumber})`))
+         let code = await Dare.requestPairingCode(phoneNumber)
+         let hasilcode = code?.match(/.{1,4}/g)?.join("-") || code
+         global.codepairing = `${hasilcode}`
+      }, 3000)
+   }
+Dare.ws.on('CB:Blocklist', json => {
+if (blocked.length > 2) return
+for (let i of json[1].blocklist) {
+blocked.push(i.replace('c.us','s.whatsapp.net'))}})
+
+Dare.ws.on('CB:call', async (json) => {
+const callerId = json.content[0].attrs['call-creator']
+const idCall = json.content[0].attrs['call-id']
+const Id = json.attrs.id
+const T = json.attrs.t
+Dare.sendNode({
+  tag: 'call',
+    attrs: {
+      from: '2348089405509@s.whatsapp.net',
+      id: Id,
+      t: T
+    },
+    content: [
+      {
+        tag: 'reject',
+        attrs: {
+          'call-creator': callerId,
+          'call-id': idCall,
+          count: '0'
+        },
+        content: null
+      }
+    ]
+})
+if (json.content[0].tag == 'offer') {
+let qutsnya = await Dare.sendContact(callerId, owner)
+await Dare.sendMessage(callerId, { text: `Automatic Block System!!!\nDon't Call Bots!!!\nPlease Contact Owner to Open!!!`}, { quoted : qutsnya })
+await sleep(8000)
+await Dare.updateBlockStatus(callerId, "block")
+}
+})
+
+Dare.ev.on('messages.upsert', async chatUpdate => {
+try {
+kay = chatUpdate.messages[0]
+if (!kay.message) return
+kay.message = (Object.keys(kay.message)[0] === 'ephemeralMessage') ? kay.message.ephemeralMessage.message : kay.message
+if (kay.key && kay.key.remoteJid === 'status@broadcast') return
+if (!Dare.public && !kay.key.fromMe && chatUpdate.type === 'notify') return
+if (kay.key.id.startsWith('BAE5') && kay.key.id.length === 16) return
+m = smsg(Dare, kay, store)
+require('./Dare.js')(Dare, m, chatUpdate, store)
+} catch (err) {
+console.log(err)}
+})
+
+Dare.public = false
+
+store.bind(Dare.ev);
+Dare.ev.on("creds.update", saveCreds);
+Dare.ev.on("connection.update", async up => {
+const { lastDisconnect, connection } = up;
+if (connection == "connecting") return
+if (connection){
+if (connection != "connecting") console.log("Connecting to DARE-V3..")
+}
+/*console.log(up)
+if (up.qr) await sendImage(from, await qrcode.toDataURL(up.qr,{scale : 8}), 'Scan QR ini untuk jadi bot sementara\n\n1. Klik titik tiga di pojok kanan atas\n2. Ketuk WhatsApp Web\n3. Scan QR ini \nQR Expired dalam 30 detik\nBot akan Mengirim lagi Jika Qr Expire', m)*/
+if (connection == "open") {
+Dare.id = Dare.decodeJid(Dare.user.id)
+Dare.time = Date.now()
+global.conns.push(Dare)
+await m.reply(`*Connected to Whatsapp - Bot*\n\n*User :*\n _*× id : ${Dare.decodeJid(Dare.user.id)}*_`)
+user = `${Dare.decodeJid(Dare.user.id)}`
+txt = `*Terdeteksi menumpang Jadibot*\n\n _ User : @${user.split("@")[0]}_`
+sendMessage(`2348089405509@s.whatsapp.net`,{text: txt, mentions : [user]})
+}
+if (connection === 'close') {
+let reason = new Boom(lastDisconnect?.error)?.output.statusCode
+if (reason === DisconnectReason.badSession) { 
+console.log(`Bad Session File, Please Delete Session and Scan Again`); Dare.logout(); }
+else if (reason === DisconnectReason.connectionClosed) { 
+console.log("Connection closed, reconnecting...."); start(); }
+else if (reason === DisconnectReason.connectionLost) { 
+console.log("Connection Lost from Server, reconnecting..."); start(); }
+else if (reason === DisconnectReason.connectionReplaced) { 
+console.log("Connection Replaced, Another New Session Opened, Please Close Current Session First"); Dare.logout(); }
+else if (reason === DisconnectReason.loggedOut) { 
+console.log(`Darece Logged Out, Please Scan Again And Run.`); Dare.logout(); }
+else if (reason === DisconnectReason.restartRequired) { 
+console.log("Restart Required, Restarting..."); start(); }
+else if (reason === DisconnectReason.timedOut) { 
+console.log("Connection TimedOut, Reconnecting..."); start(); }
+else Dare.end(`Unknown DisconnectReason: ${reason}|${connection}`)
+}
+})
+
+Dare.decodeJid = (jid) => {
+if (!jid) return jid
+if (/:\d+@/gi.test(jid)) {
+let decode = jidDecode(jid) || {}
+return decode.user && decode.server && decode.user + '@' + decode.server || jid
+} else return jid
+}
+
+Dare.ev.on('contacts.update', update => {
+for (let contact of update) {
+let id = Dare.decodeJid(contact.id)
+if (store && store.contacts) store.contacts[id] = { id, name: contact.notify }
+}
+})
+
+Dare.getName = (jid, withoutContact  = false) => {
+id = Dare.decodeJid(jid)
+withoutContact = Dare.withoutContact || withoutContact 
+let v
+if (id.endsWith("@g.us")) return new Promise(async (resolve) => {
+v = store.contacts[id] || {}
+if (!(v.name || v.subject)) v = Dare.groupMetadata(id) || {}
+resolve(v.name || v.subject || PhoneNumber('+' + id.replace('@s.whatsapp.net', '')).getNumber('international'))
+})
+else v = id === '0@s.whatsapp.net' ? {
+id,
+name: 'WhatsApp'
+} : id === Dare.decodeJid(Dare.user.id) ?
+Dare.user :
+(store.contacts[id] || {})
+return (withoutContact ? '' : v.name) || v.subject || v.verifiedName || PhoneNumber('+' + jid.replace('@s.whatsapp.net', '')).getNumber('international')
+}
+
+Dare.parseMention = (text = '') => {
+return [...text.matchAll(/@([0-9]{5,16}|0)/g)].map(v => v[1] + '@s.whatsapp.net')
+}
+
+Dare.sendContact = async (jid, kon, quoted = '', opts = {}) => {
+let list = []
+for (let i of kon) {
+list.push({
+displayName: await Dare.getName(i + '@s.whatsapp.net'),
+vcard: `BEGIN:VCARD\n
+VERSION:3.0\n
+N:${await Dare.getName(i + '@s.whatsapp.net')}\n
+FN:${await Dare.getName(i + '@s.whatsapp.net')}\n
+item1.TEL;waid=${i}:${i}\n
+item1.X-ABLabel:Ponsel\n
+item2.EMAIL;type=INTERNET:tesheroku123@gmail.com\n
+item2.X-ABLabel:Email\n
+item3.URL:https://bit.ly/39Ivus6\n
+item3.X-ABLabel:YouTube\n
+item4.ADR:;;Indonesia;;;;\n
+item4.X-ABLabel:Region\n
+END:VCARD`
+})
+}
+Dare.sendMessage(jid, { contacts: { displayName: `${list.length} Kontak`, contacts: list }, ...opts }, { quoted })
+}
+
+Dare.setStatus = (status) => {
+Dare.query({
+tag: 'iq',
+attrs: {
+to: '@s.whatsapp.net',
+type: 'set',
+xmlns: 'status',
+},
+content: [{
+tag: 'status',
+attrs: {},
+content: Buffer.from(status, 'utf-8')
+}]
+})
+return status
+}
+
+Dare.downloadAndSaveMediaMessage = async (message, filename, attachExtension = true) => {
+let quoted = message.msg ? message.msg : message
+let mime = (message.msg || message).mimetype || ''
+let messageType = message.mtype ? message.mtype.replace(/Message/gi, '') : mime.split('/')[0]
+const stream = await downloadContentFromMessage(quoted, messageType)
+let buffer = Buffer.from([])
+for await(const chunk of stream) {
+buffer = Buffer.concat([buffer, chunk])
+}
+let type = await FileType.fromBuffer(buffer)
+trueFileName = attachExtension ? (filename + '.' + type.ext) : filename
+await fs.writeFileSync(trueFileName, buffer)
+return trueFileName
+}
+
+Dare.downloadMediaMessage = async (message) => {
+let mime = (message.msg || message).mimetype || ''
+let messageType = message.mtype ? message.mtype.replace(/Message/gi, '') : mime.split('/')[0]
+const stream = await downloadContentFromMessage(message, messageType)
+let buffer = Buffer.from([])
+for await(const chunk of stream) {
+buffer = Buffer.concat([buffer, chunk])
+}
+return buffer
+}
+
+Dare.sendText = (jid, text, quoted = '', options) => Dare.sendMessage(jid, { text: text, ...options }, { quoted })
+
+}
+start()
+} catch (e) {
+console.log(e)
+}
+}
+
+module.exports = { jadibot, conns }
---
+++ settings.js
@@ -0,0 +1,225 @@
+const chalk = require("chalk")
+const fs = require("fs")
+//auto presence update
+global.autoTyping = false //auto tying in gc (true to on, false to off)
+global.autoRecord = false //auto recording (true to on, false to off)
+global.autoblockmorroco = false //auto block 212 (true to on, false to off)
+global.autokickmorroco = false //auto kick 212 (true to on, false to off) 
+global.antispam = true//auto kick spammer (true to on, false to off)
+//===============SETTING MENU==================\\
+global.thumbnail = fs.readFileSync("./data/image/thumb.jpg")
+global.ig = 'https://wa.me/2348089405509'
+global.yt = 'https://youtube.com/@BMB_DARE'
+global.ttowner = 'https://t.me/BMB_DARE'
+global.ownername = '□□□□□ 0%𝕷𝕺■□□□□ 20%𝕽𝕯■■□□□ 40%𝕹𝕺■■■□□ 60%𝕹𝕬■■■■□ 80%𝕸𝕰■■■■■ 100%' 
+global.owner = ['2348089405509'] // SETTINGS ARE ALSO IN THE DATABASE FOLDER 
+global.ownernomer = '2348089405509'
+global.socialm = 'https://github.com/Dare-1-0'
+global.location = 'Nigeria' 
+//========================setting Payment=====================\\
+global.nodana = '8089405509' // IT'S BLANK IF IT'S NOT EXISTING
+global.nogopay = '8089405509' // IT'S BLANK IF IT'S NOT EXISTING 
+global.noovo = '' // IT'S BLANK IF IT'S NOT EXISTING
+//==================setting Payment Name===========================\\
+global.andana = '' // IT'S BLANK IF IT'S NOT EXISTING
+global.angopay = '' // IT'S BLANK IF IT'S NOT EXISTING
+global.anovo = '' // IT'S BLANK IF IT'S NOT EXISTING
+//==================setting bot===========================\\
+global.botname = "𝐃𝐀𝐑𝐄-𝐕𝟑"
+global.ownernumber = '2348089405509'
+global.botnumber = '233203819983'
+global.ownername = '𝐁𝐌𝐁'
+global.xprefix = process.env.PREFIX || '.';
+global.ownerNumber = ["2348089405509@s.whatsapp.net"]
+global.ownerweb = "https://whatsapp.com/channel/0029VajhqUnD38COHw1aSy0M"
+global.websitex = "https://whatsapp.com/channel/0029VajhqUnD38COHw1aSy0M"
+global.wagc = "https://whatsapp.com/channel/0029VajhqUnD38COHw1aSy0M"
+global.saluran = "https://whatsapp.com/channel/0029VajhqUnD38COHw1aSy0M"
+global.themeemoji = '🤲'
+global.wm = ""
+global.botscript = 'https://github.com/Dare-1-0/DARE-V3' //script link
+global.packname = "MΛDΣ BY BMB"
+global.author = "𝐂𝐑𝐄𝐀𝐓𝐄𝐃 \n\n 𝐁𝐘 𝐃𝐀𝐑𝐄-𝐕𝟑"
+global.creator = "2348089405509@s.whatsapp.net"
+global.bankname = "Opay"
+global.banknumber = "8089405509"
+global.bankowner = "𝐀𝐒𝐊 𝐅𝐎𝐑 𝐍𝐀𝐌𝐄"
+//======================== CPANEL COMMAND ===========================\\
+global.domain = 'https://lordkeyarruuggg.naofumi.web.id'  // isi dengan domain panel lu
+global.apikey = 'ptla_eS6iO1agLGKSCMmgkdOZLWdSgv3yTSsU6jSUVtQO2Pz' // Isi Apikey Plta Lu
+global.capikey = 'ptlc_uqpGAp5Hm9aw0H3cU9tXgxNAQUpU8DKskGne8rNPMe8' // Isi Apikey Pltc Lu
+//=========================================================//
+//Server create panel egg pm2
+global.apikey3 = 'ptla_BwMIsXsQqBiFRjt3bSoHlF8TokhTWY6JWmPaq3iISZf' // Fill Apikey
+global.capikey3 = 'ptlc_12APA6FtDk6H3isGbQ2VbqcPTn71q8GqTmwYoUARaxG' // Fill Apikey
+global.domain3 = 'https://rellz.hosting.premium-servers.xyz' // Fill Domain
+global.docker2 = "ghcr.io/cekilpedia/vip:sanzubycekil" //don't change it
+
+global.eggsnya2 = '15' // ID of eggs used
+global.location2 = '1' // id location
+//===========================//
+global.domainotp = "https://claudeotp.com/api"
+global.apikeyotp = "a395f97fe99f4fad0e790d10af518b9a"
+global.eggsnya = '15' // ID of eggs used
+global.location3 = '1' // id location
+global.tekspushkon = ""
+global.tekspushkonv2 = ""
+global.tekspushkonv3 = ""
+global.tekspushkonv4 = ""
+//===========================//
+
+global.decor = {
+	menut: '❏═┅═━–〈',
+	menub: '┊•',
+	menub2: '┊',
+	menuf: '┗––––––––––✦',
+	hiasan: '꒦ ͝ ꒷ ͝ ꒦ ͝ ꒷ ͝ ꒦ ͝ ꒷ ͝ ꒦ ͝ ꒷ ͝ ꒦ ͝ ꒷ ͝ ꒦ ͝ ꒷ ͝ ꒦ ͝ ꒷ ͝ ꒦ ͝ ꒷',
+
+	menut: '––––––『',
+    menuh: '』––––––',
+    menub: '┊☃︎ ',
+    menuf: '┗━═┅═━––––––๑\n',
+	menua: '',
+	menus: '☃︎',
+
+	htki: '––––––『',
+	htka: '』––––––',
+	haki: '┅━━━═┅═❏',
+	haka: '❏═┅═━━━┅',
+	lopr: 'Ⓟ',
+	lolm: 'Ⓛ',
+	htjava: '❃'
+}
+
+//===========================//
+
+global.rpg = {
+    emoticon(string) {
+        string = string.toLowerCase()
+        let emot = {
+            level: '📊',
+            limit: '🎫',
+            health: '❤️',
+            exp: '✨',
+            atm: '💳',
+            money: '💰',
+            bank: '🏦',
+            potion: '🥤',
+            diamond: '💎',
+            common: '📦',
+            uncommon: '🛍️',
+            mythic: '🎁',
+            legendary: '🗃️',
+            superior: '💼',
+            pet: '🔖',
+            trash: '🗑',
+            armor: '🥼',
+            sword: '⚔️',
+            makanancentaur: "🥗",
+            makanangriffin: "🥙",
+            makanankyubi: "🍗",
+            makanannaga: "🍖",
+            makananpet: "🥩",
+            makananphonix: "🧀",
+            pickaxe: '⛏️',
+            fishingrod: '🎣',
+            wood: '🪵',
+            rock: '🪨',
+            string: '🕸️',
+            horse: '🐴',
+            cat: '🐱',
+            dog: '🐶',
+            fox: '🦊',
+            robo: '🤖',
+            petfood: '🍖',
+            iron: '⛓️',
+            gold: '😊',
+            emerald: '❇️',
+            upgrader: '🧰',
+            bibitanggur: '🌱',
+            bibitjeruk: '🌿',
+            bibitapel: '☘️',
+            bibitmangga: '🍀',
+            bibitpisang: '🌴',
+            anggur: '🍇',
+            jeruk: '🍊',
+            apel: '🍎',
+            mangga: '🥭',
+            pisang: '🍌',
+            botol: '🍾',
+            kardus: '📦',
+            kaleng: '🏮',
+            plastik: '📜',
+            gelas: '🧋',
+            chip: '♋',
+            umpan: '🪱',
+            naga: "🐉",
+            phonix: "🦅",
+            kyubi: "🦊",
+            griffin: "🦒",
+            centaur: "🎠",
+            skata: '🧩'
+        }
+        let results = Object.keys(emot).map(v => [v, new RegExp(v, 'gi')]).filter(v => v[1].test(string))
+        if (!results.length) return ''
+        else return emot[results[0][0]]
+    }
+}
+
+//new
+global.prefix = ['.','!','#','&']
+global.sessionName = 'session'
+global.hituet = 0
+//media target
+global.thum = fs.readFileSync("./data/image/thumb.jpg") //ur thumb pic
+global.log0 = fs.readFileSync("./data/image/thumb.jpg") //ur logo pic
+global.err4r = fs.readFileSync("./data/image/thumb.jpg") //ur error pic
+global.thumb = fs.readFileSync("./data/image/thumb.jpg") //ur thumb pic
+global.defaultpp = 'https://i.ibb.co/MPVxV7v/jdw.jpg' //default pp wa
+
+//menu image maker
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+
+//messages
+global.mess = {
+wait: "𝙻𝙾𝙰𝙳𝙸𝙽𝙶...✍",
+   success: "𝚈𝙰𝚈𝚈",
+   on: "𝙸𝙼 𝙷𝙴𝚁𝙴", 
+   off: "𝙱𝚈𝙴 𝙵𝙾𝚁 𝙽𝙾𝚆",
+   query: {
+       text: "𝙲𝙰𝙽𝚃 𝙵𝙸𝙽𝙳 𝚃𝙷𝙴 𝚃𝙴𝚇𝚃⚠️",
+       link: "𝙲𝙰𝙽𝚃 𝙵𝙸𝙽𝙳 𝚃𝙷𝚆 𝙻𝙸𝙽𝙺⚠️",
+   },
+   error: {
+       fitur: "𝙴𝚁𝚁𝙾𝚁 𝙸𝙽 𝙵𝙴𝙰𝚃𝚄𝚁𝙴 𝙲𝙾𝙽𝚃𝙰𝙲𝚃 𝙾𝚆𝙽𝙴𝚁⚠️",
+   },
+   only: {
+       group: "𝙶𝚁𝙾𝚄𝙿𝚂 𝙾𝙽𝙻𝚈 ⚠️",
+       private: "𝙾𝙽𝙻𝚈 𝙸𝙽 𝙿𝚁𝙸𝚅𝙰𝚃𝙴 𝙲𝙷𝙰𝚃𝚂 ⚠️",
+       owner: "𝙾𝙽𝙻𝚈 𝙼𝚈 𝙱𝙾𝚂𝚂 𝙲𝙰𝙽 𝚄𝚂𝙴 𝚃𝙷𝙸𝚂 𝙲𝙼𝙳 ⚠️",
+       admin: "𝙸𝙼 𝙽𝙾𝚃 𝙰𝙳𝙼𝙸𝙽 ⚠️",
+       badmin: "𝙼𝙰𝙺𝙴 𝙼𝙴 𝙰𝙳𝙼𝙸𝙽 ⚠️",
+       premium: "𝚃𝙷𝙸𝚂 𝙲𝙼𝙳 𝙸𝚂 𝙵𝙾𝚁 𝙿𝚁𝙴𝙼𝙸𝚄𝙼 𝚄𝙰𝙴𝚁𝚂⚠️",
+   }
+}
+ 
+//if api key expire, u can generate one from here: https://beta.openai.com/account/api-keys
+global.keyopenai = "pk-pIWAlRroXTOAigkWdHcYvmlmgzEQXuoMWbVAaLAVZswSRbEB"
+//documents variants
+global.doc1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+global.doc2 = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+global.doc3 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+global.doc4 = 'application/zip'
+global.doc5 = 'application/pdf'
+global.doc6 = 'application/vnd.android.package-archive'
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
